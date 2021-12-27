d615466247200f0f1ec0664fdd7a4a1d94a240f8
thanhlam <lam.nguyen@pascal.studio>
2021-12-27 15:40:27 +0700

    init server and client

---
+++ server.js
@@ -0,0 +1,6 @@
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
