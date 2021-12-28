a7b13c10244fe1b9578c66001a8d07d6ba1647d5
thanhlam <lam.nguyen@pascal.studio>
2021-12-28 15:39:02 +0700

    add protected route, login, forgot password, reset password and send email for reset token

---
+++ controllers/private.js
@@ -0,0 +1,6 @@
+exports.getPrivateData = (req, res, next) => {
+  return res.status(200).json({
+    success: true,
+    data: "You got access to the private data in this route",
+  });
+};
--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+
+// Connect DB
+connectDB();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/private", require("./routes/private"));
+
+// Error Handler (Should be last piece of middleware)
+app.use(errorHandler);
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+process.on("unhandledRejection", (err, _) => {
+  console.log(`Logged Error: ${err}`);
+  server.close(() => process.exit(1));
+});
